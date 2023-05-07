2d74d2a5fe832256a871387ed91da702184a2ac0
issac <lsls4868@gmail.com>
2023-05-08 03:30:25 +0900

    feat : SignRepository

--- app/src/main/java/org/android/go/sopt/domain/SignRepository.kt
+++ app/src/main/java/org/android/go/sopt/domain/SignRepository.kt
@@ -1,7 +1,13 @@
 package org.android.go.sopt.domain
 
+import org.android.go.sopt.model.RequestSignInDto
+import org.android.go.sopt.model.RequestSignUpDto
+import org.android.go.sopt.model.ResponseSignUpDto
+import retrofit2.Response
 import javax.inject.Singleton
 
 @Singleton
 interface SignRepository {
+    suspend fun singUp(requestSignUpDto: RequestSignUpDto): Response<ResponseSignUpDto>
+    suspend fun singIn(requestSignInDto: RequestSignInDto): Response<ResponseSignUpDto>
 }
