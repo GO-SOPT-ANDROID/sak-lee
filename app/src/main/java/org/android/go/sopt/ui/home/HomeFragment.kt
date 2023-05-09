b356f9f5f52f1ec5b5c473d6313776c46c54b728
issac <lsls4868@gmail.com>
2023-05-10 01:30:19 +0900

    feat : submit data

--- app/src/main/java/org/android/go/sopt/ui/home/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/ui/home/HomeFragment.kt
@@ -1,25 +1,24 @@
 package org.android.go.sopt.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.selection.SelectionPredicates
-import androidx.recyclerview.selection.SelectionTracker
-import androidx.recyclerview.selection.StableIdKeyProvider
-import androidx.recyclerview.selection.StorageStrategy
-import com.google.gson.Gson
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.PagingData
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.android.go.sopt.R
+import org.android.go.sopt.data.model.ResponseUserInfo
 import org.android.go.sopt.databinding.FragmentHomeBinding
-import org.android.go.sopt.data.model.FakeGithubInfo
-import org.android.go.sopt.ui.join.JoinViewModel
-import org.android.go.sopt.util.AssetLoader
+
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private val viewModel by viewModels<HomeViewModel>()
@@ -27,21 +26,7 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    private val adapter = HomeAdapter()
-
-    private val selectionTracker by lazy {
-        with(binding) {
-            SelectionTracker.Builder(
-                "selection_id",//객체의 ID
-                rvFakeGithubInfo, // 적용될 Rv
-                StableIdKeyProvider(rvFakeGithubInfo),//ItemKeyProvider 라이브러리에서 제공.커스텀 필요하는 경우 있음
-                HomeAdapter.HomeLookUp(rvFakeGithubInfo), //  RecyclerView 아이템의 대한 정보
-                StorageStrategy.createLongStorage()//선택항목 저장 전략 라이브러리 제공
-            )
-                .withSelectionPredicate(SelectionPredicates.createSelectAnything())
-                .build()
-        }
-    }
+    private val adapter = HomePagingAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,31 +40,34 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val fakeGithubAsset = loadFakeGithubInfo()
-
-        initAdapter(fakeGithubAsset)
+        initAdapter()
 
     }
 
-    private fun loadFakeGithubInfo(): FakeGithubInfo? {
-        val asserLoader = AssetLoader(requireContext())
-        val fakeGithubAssetLoader = asserLoader.getJsonString("fake_repo_list.json")
-        val gson = Gson()
-        viewModel.getUserList()
-        return gson.fromJson(fakeGithubAssetLoader, FakeGithubInfo::class.java)
-    }
-
-    private fun initAdapter(fakeGithubAsset: FakeGithubInfo?) {
+    private fun initAdapter() {
         binding.rvFakeGithubInfo.adapter = adapter.apply {
-            submitList(fakeGithubAsset)
+            productSubmitData(adapter, viewModel.getUserList())
         }
-        adapter.setSelectionTracker(selectionTracker)
+    }
 
+    private fun productSubmitData(
+        pagingAdapter: HomePagingAdapter,
+        getData: Flow<PagingData<ResponseUserInfo>>
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    getData.collectLatest { pagingData ->
+                        pagingAdapter.submitData(pagingData)
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 
 
