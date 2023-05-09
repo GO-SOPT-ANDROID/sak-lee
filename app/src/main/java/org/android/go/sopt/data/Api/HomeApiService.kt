d321def72f177a2c82503f9bfd32f5755d949185
issac <lsls4868@gmail.com>
2023-05-10 01:28:57 +0900

    feat : adpater

--- app/src/main/java/org/android/go/sopt/data/Api/HomeApiService.kt
+++ app/src/main/java/org/android/go/sopt/data/Api/HomeApiService.kt
@@ -2,11 +2,9 @@ package org.android.go.sopt.data.Api
 
 
 import org.android.go.sopt.data.model.UserList
-import org.android.go.sopt.util.User
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.security.cert.CertStoreSpi
 
 interface HomeApiService {
 
---
+++ app/src/main/java/org/android/go/sopt/ui/home/HomePagingAdapter.kt
@@ -0,0 +1,45 @@
+package org.android.go.sopt.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import org.android.go.sopt.data.model.ResponseUserInfo
+import org.android.go.sopt.databinding.ItemGithubInfoBinding
+
+class HomePagingAdapter (
+) : PagingDataAdapter<ResponseUserInfo, HomePagingAdapter.PagingViewHolder>(
+    HomeDiffCallback()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagingViewHolder {
+        val binding = ItemGithubInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PagingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PagingViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    inner class PagingViewHolder(private val binding: ItemGithubInfoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data:ResponseUserInfo) {
+            binding.responseUserInfo = data
+            binding.executePendingBindings()
+        }
+    }
+
+}
+
+class HomeDiffCallback : DiffUtil.ItemCallback<ResponseUserInfo>() {
+    override fun areItemsTheSame(oldItem:ResponseUserInfo, newItem: ResponseUserInfo): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ResponseUserInfo, newItem: ResponseUserInfo): Boolean {
+        return oldItem == newItem
+    }
+
+}
