dadf41a586d4a3d8ff12840997f19dd088bbe5e2
issac <lsls4868@gmail.com>
2023-05-10 15:40:06 +0900

    feat : Qualifier

---
+++ app/src/main/java/org/android/go/sopt/di/Qualifier.kt
@@ -0,0 +1,11 @@
+package org.android.go.sopt.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class SoptRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ReqresRetrofit
